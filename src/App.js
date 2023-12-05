d7b9dea5b6681d60b68f76789029070d09c18dff
MatheusEli <matheuselisouzaferreira@hotmail.com>
2023-12-05 18:05:22 -0300

    Languages & Projects section config

--- src/App.js
+++ src/App.js
@@ -1,57 +1,131 @@
 import "./sass/main.scss";
-import React from 'react'
+import NatoursImage from './assets/natours.png';
+import TEMSImage from './assets/tems.png';
+import SOSTEMSImage from './assets/sostems.png';
+import NexterImage from './assets/nexter.png';
+import TrilloImage from './assets/trillo.png';
 
 export default function App() {
   return (
-    <div className="container" style={{height: '100vh'}}>
-        <section className='header'>
-          <nav className='navigation'>
-
-          </nav>
-
-          <h1>Nice to meet you!<br/><span>I'm Matheus Eli</span></h1>
-          <p>Based in the UK, I’m a front-end developer passionate about building accessible web apps that users love.</p>
-        
-
-          <h6>CONTACT ME</h6>
-        </section>
-
-        <section className='languages'>
+    <div className="container">
+      <section className="header">
+        <nav className="navigation">
+          <a href="#">matheuseli</a>
           <ul>
             <li>
-              <h4>HTML</h4>
-              <p>4 Years Experience</p>
+              <a href="#">
+                <img src="github.svg" />
+              </a>
             </li>
             <li>
-              <h4>HTML</h4>
-              <p>4 Years Experience</p>
+              <a href="#">
+                <img src="mentor.svg" />
+              </a>
             </li>
             <li>
-              <h4>HTML</h4>
-              <p>4 Years Experience</p>
+              <a href="#">
+                <img src="linkedin.svg" />
+              </a>
             </li>
             <li>
-              <h4>HTML</h4>
-              <p>4 Years Experience</p>
-            </li>
-            <li>
-              <h4>HTML</h4>
-              <p>4 Years Experience</p>
-            </li>
-            <li>
-              <h4>HTML</h4>
-              <p>4 Years Experience</p>
+              <a href="#">
+                <img src="twitter.svg" />
+              </a>
             </li>
           </ul>
-        </section>
+        </nav>
 
-        <section className='projects'>
+        <div className="header__text-box">
+          <h1>
+            Nice to meet you!
+            <br />
+            I'm <span>Matheus Eli</span>.
+          </h1>
+          <p>
+            Based in the São Paulo, I’m a front-end developer passionate about
+            building accessible web apps that users love.
+          </p>
 
-        </section>
+          <h6>CONTACT ME</h6>
+        </div>
+      </section>
 
-        <section className='contact'>
+      <section className="languages">
+        <ul className="languages__list">
+          <li className="languages__item">
+            <h4>HTML</h4>
+            <p>4 Years Experience</p>
+          </li>
+          <li className="languages__item">
+            <h4>CSS</h4>
+            <p>4 Years Experience</p>
+          </li>
+          <li className="languages__item">
+            <h4>Javascript</h4>
+            <p>4 Years Experience</p>
+          </li>
+          <li className="languages__item">
+            <h4>Acessibility</h4>
+            <p>1 Years Experience</p>
+          </li>
+          <li className="languages__item">
+            <h4>React</h4>
+            <p>1 Year Experience</p>
+          </li>
+          <li className="languages__item">
+            <h4>Sass</h4>
+            <p>2 Years Experience</p>
+          </li>
+        </ul>
+      </section>
 
-        </section>
+      <section className="projects">
+        <h2>Projects</h2>
+        <h6>CONTACT ME</h6>
+
+        <ul>
+          <li>
+            <figure>
+              <img src={NatoursImage}  alt="Natours Website"/>
+              <figcaption>
+                <h5>Natours</h5>
+                HTML SASS
+              </figcaption>
+            </figure>
+          </li>
+          <li>
+            <figure>
+              <img src={TEMSImage}  alt="Natours Website"/>
+              <figcaption>
+                <h5>TEMS</h5>
+                REACT HTML SASS JAVASCRIPT
+              </figcaption>
+            </figure>
+          </li>
+          <li>
+            <figure>
+              <img src={NexterImage}  alt="Natours Website"/>
+              <figcaption>
+                <h5>Nexter</h5>
+                HTML SASS
+              </figcaption>
+            </figure>
+          </li>
+          <li>
+            <figure>
+              <img src={SOSTEMSImage}  alt="Natours Website"/>
+              <figcaption>
+                <h5>SOS TEMS</h5>
+                ANGULAR HTML SASS TYPESCRIPT
+              </figcaption>
+            </figure>
+          </li>
+        </ul>
+      </section>
+
+      <section className="contact">
+
+      </section>
     </div>
-  )
+  );
 }
