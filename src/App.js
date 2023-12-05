4bd0b9df28c310a66872edcba05dd70ff5b5f55a
MatheusEli <matheuselisouzaferreira@hotmail.com>
2023-12-04 22:22:26 -0300

    Header Footer folder config

--- src/App.js
+++ src/App.js
@@ -3,8 +3,55 @@ import React from 'react'
 
 export default function App() {
   return (
-    <div className="container">
-        <div style={{height: '100vh'}}></div>
+    <div className="container" style={{height: '100vh'}}>
+        <section className='header'>
+          <nav className='navigation'>
+
+          </nav>
+
+          <h1>Nice to meet you!<br/><span>I'm Matheus Eli</span></h1>
+          <p>Based in the UK, I’m a front-end developer passionate about building accessible web apps that users love.</p>
+        
+
+          <h6>CONTACT ME</h6>
+        </section>
+
+        <section className='languages'>
+          <ul>
+            <li>
+              <h4>HTML</h4>
+              <p>4 Years Experience</p>
+            </li>
+            <li>
+              <h4>HTML</h4>
+              <p>4 Years Experience</p>
+            </li>
+            <li>
+              <h4>HTML</h4>
+              <p>4 Years Experience</p>
+            </li>
+            <li>
+              <h4>HTML</h4>
+              <p>4 Years Experience</p>
+            </li>
+            <li>
+              <h4>HTML</h4>
+              <p>4 Years Experience</p>
+            </li>
+            <li>
+              <h4>HTML</h4>
+              <p>4 Years Experience</p>
+            </li>
+          </ul>
+        </section>
+
+        <section className='projects'>
+
+        </section>
+
+        <section className='contact'>
+
+        </section>
     </div>
   )
 }
